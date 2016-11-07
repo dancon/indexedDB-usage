7b7a6953e76935c4288099fd5351010a4741b12c
John Hou <870301137@qq.com>
2016-11-08 01:17:25 +0800

    indexed 添加，获取数据

--- src/database.js
+++ src/database.js
@@ -6,12 +6,20 @@ if(!window.indexedDB){
     console.log('Your browser is not support indexedDB');
 }
 
+var curtomerData = [{
+    ssn: '444-444-444', name: 'Bill', age: 34, email: 'bill@company.com'
+}, {
+    ssn: '555-555-555', name: 'Donna', age: 32, email: 'donna@home.org'
+}], db;
+
 // 通过 open 函数来打开一个指定版本的数据库， open 函数的第二个参数指定了数据库的版本号，而且数据库的版本号必须是一个 int 型的整数，不可以是 float, 如果你指定了 float 类型的 version, indexedDB 会默认向下取整
 
-var request = window.indexedDB.open('MyTestDatabase', 3);
+var request = window.indexedDB.open('the_name', 4);
 
 request.onerror = function(event){
     console.log('打开 DB 失败', event);
+
+    db = event.target.result;
 };
 
 
@@ -19,4 +27,45 @@ request.onerror = function(event){
 request.onsuccess = function(event){
     console.log('打开 DB 成功', event, request);
     console.log(this === request);
+
+};
+    
+request.onupgradeneeded = function(event){
+    console.log('upgrade DB');
+  var db = event.target.result,
+    objectStore;
+
+    console.log(db);
+
+     if(!db.objectStoreNames.contains('curstomers')){
+         objectStore = db.createObjectStore('curstomers', {keyPath: 'ssn'});
+     }else{
+         objectStore = db.transaction(['curstomers']).objectStore('curstomers');
+     }
+
+     objectStore.createIndex('name', 'name', {unique: false});
+     objectStore.createIndex('email', 'email', {unique: true});
+
+     objectStore.transaction.oncomplete = function(event){
+        var curstomerObjectStore = db.transaction('curstomers', 'readwrite').objectStore('curstomers');
+
+        for(var i in curtomerData){
+            curstomerObjectStore.add(curtomerData[i]);
+        }
+
+        console.log('完成添加数据');
+
+        getData('444-444-444');
+     };  
+
+};
+
+function getData(key){
+    var t = db.transaction(['curstomers']),
+        dbStore = t.objectStore('curstomers'),
+        request = dbStore.get(key);
+
+    request.onsuccess = function(event){
+       console.log('Name of', key, ' is', request.result.name);  
+    };
 }
