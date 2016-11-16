dd9a116f3fd9e17b68635344091100e3bde4cbae
houquan <houquan@zuoyebang.com>
2016-11-16 20:19:25 +0800

    indexedDB

--- src/indexedDB.js
+++ src/indexedDB.js
@@ -2,3 +2,24 @@
 var idb = window.indexedDB || window.webkitIndexedDB,
   db;
 
+function createTable(dbInfo, tablename){
+
+  return new Promise(function(resolve, reject){
+
+    var req = indexedDB.open(dbInfo.name, dbInfo.version),
+      hasUpgrade = false;
+
+    req.onupgradeneeded = function(event){
+      var db = req.result;
+      console.log('up');
+    }
+
+    req.onsuccess = function(){
+      console.log('open success');
+      var db = req.result;
+      resolve(db);
+    }
+  });
+}
+
+createTable('test', 'table1');
